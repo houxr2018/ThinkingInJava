fe5b2bc2010e678a9794fbf93094e3f74a79d7f6
wangxn <704801812@qq.com>
2017-10-10 10:13:58 +0800

    练习2.4

---
+++ src/main/java/chapter2/section4/DataOnly.java
@@ -0,0 +1,20 @@
+package chapter2.section4;
+
+/**
+ * @Description DataOnly完整代码
+ * @CreateUser 王熙楠 wangxn@tiansu-china.com
+ * @CreateDate 2017/10/10 10:10
+ */
+public class DataOnly {
+
+    private int i;
+    private double d;
+    private boolean bool;
+
+    public static void main(String[] args) {
+        DataOnly data = new DataOnly();
+        data.i = 47;
+        data.d = 1.1;
+        data.bool = false;
+    }
+}
