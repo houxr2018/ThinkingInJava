3532a4e88e32ddcccdfd7234be58e3c2b6411abc
wangxn <704801812@qq.com>
2017-10-10 11:06:01 +0800

    练习2.8

---
+++ src/main/java/chapter2/section8/StaticTest.java
@@ -0,0 +1,23 @@
+package chapter2.section8;
+
+/**
+ * @Description 展示一个类的多个对象共用同一个static作用域
+ * @CreateUser 王熙楠 wangxn@tiansu-china.com
+ * @CreateDate 2017/10/10 10:55
+ */
+public class StaticTest {
+
+    private static int i = 47;
+
+    public static void main(String[] args) {
+
+        StaticTest test1 = new StaticTest();
+        StaticTest test2 = new StaticTest();
+
+        System.out.println("test1.i = " + test1.i);
+        System.out.println("test2.i = " + test2.i++);
+        System.out.println("test1.i = " + test1.i);
+        System.out.println("test2.i = " + test2.i);
+
+    }
+}
