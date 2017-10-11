f93a9c3890761efcb11f890645861a3757d6642b
wangxn <704801812@qq.com>
2017-10-11 16:40:54 +0800

    练习3.1

---
+++ src/main/java/chapter3/section1/PrintShort.java
@@ -0,0 +1,17 @@
+package chapter3.section1;
+
+
+import static net.mindview.util.Print.*;
+
+/**
+ * Description: 使用简短的和正常的方法打印语句
+ * CreateTime: 2017/10/11 15:58
+ *
+ * @author 王熙楠 wangxn@tiansu-china.com
+ */
+public class PrintShort {
+    public static void main(String[] args) {
+        print("short print");
+        System.out.println("normal print");
+    }
+}
