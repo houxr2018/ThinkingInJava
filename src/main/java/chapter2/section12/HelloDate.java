309493f5b5fd5828ac57388868ed388d139d835c
wangxn <704801812@qq.com>
2017-10-11 13:59:26 +0800

    练习2.12

---
+++ src/main/java/chapter2/section12/HelloDate.java
@@ -0,0 +1,19 @@
+package chapter2.section12;
+
+import java.util.Date;
+
+/**
+ * @Description 测试javadoc
+ * @CreateUser 王熙楠 wangxn@tiansu-china.com
+ * @CreateDate 2017/10/10 21:01
+ */
+public class HelloDate {
+    /** Entry point to class & application.
+     * @param args array of string arguments
+     * @throws exceptions No exceptions thrown
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello, it's: ");
+        System.out.println(new Date());
+    }
+}
