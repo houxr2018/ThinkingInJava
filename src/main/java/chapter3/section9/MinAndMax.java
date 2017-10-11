c8f0e81a636571ddfdeff62c4f20fea09a3bafbd
wangxn <704801812@qq.com>
2017-10-11 19:51:02 +0800

    练习3.9

---
+++ src/main/java/chapter3/section9/MinAndMax.java
@@ -0,0 +1,18 @@
+package chapter3.section9;
+
+/**
+ * Description: 显示float和double的最大最小值
+ * CreateTime: 2017/10/11 19:46
+ *
+ * @author 王熙楠 wangxn@tiansu-china.com
+ */
+public class MinAndMax {
+
+    public static void main(String[] args) {
+
+        System.out.println("float的最大值：" + Float.MAX_VALUE);
+        System.out.println("float的最小值：" + Float.MIN_VALUE);
+        System.out.println("double的最大值：" + Double.MAX_VALUE);
+        System.out.println("double的最小值：" + Double.MIN_VALUE);
+    }
+}
