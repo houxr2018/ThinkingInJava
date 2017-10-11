6868ab17056e0d6142523cfaa9965d2a04c97a87
wangxn <704801812@qq.com>
2017-10-11 18:39:14 +0800

    练习3.5

---
+++ src/main/java/chapter3/section5/TheDogs.java
@@ -0,0 +1,29 @@
+package chapter3.section5;
+
+/**
+ * Description:
+ * CreateTime: 2017/10/11 18:32
+ *
+ * @author 王熙楠 wangxn@tiansu-china.com
+ */
+class Dogs {
+
+    public String name;
+    public String says;
+
+    public Dogs(String name, String says) {
+        this.name = name;
+        this.says = says;
+    }
+}
+
+public class TheDogs {
+
+    public static void main(String[] args) {
+
+        Dogs spot = new Dogs("spot", "Ruff!");
+        Dogs scruffy = new Dogs("scruffy", "Wurf!");
+        System.out.println("1: name: " + spot.name + ", says: " + spot.says);
+        System.out.println("2: name: " + scruffy.name + ", says: " + scruffy.says);
+    }
+}
