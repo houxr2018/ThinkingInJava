c479444ff7888acabd160297519274456b65fe8d
wangxn <704801812@qq.com>
2017-10-12 16:36:19 +0800

    练习4.1

---
+++ src/main/java/chapter4/section1/OneToOneHundred.java
@@ -0,0 +1,17 @@
+package chapter4.section1;
+
+/**
+ * Description: 打印从1到100的数字
+ * CreateTime: 2017/10/12 16:34
+ *
+ * @author 王熙楠 wangxn@tiansu-china.com
+ */
+public class OneToOneHundred {
+
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 100; i++) {
+            System.out.print(i + " ");
+        }
+    }
+}
