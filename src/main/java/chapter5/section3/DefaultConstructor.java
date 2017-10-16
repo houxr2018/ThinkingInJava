0a942cba8a17c9ae936849fb5331aa6d3fdfdfec
wangxn <704801812@qq.com>
2017-10-16 14:10:03 +0800

    练习5.3

---
+++ src/main/java/chapter5/section3/DefaultConstructor.java
@@ -0,0 +1,20 @@
+package chapter5.section3;
+
+/**
+ * Description: 无参构造器
+ * CreateTime: 2017/10/16 14:04
+ *
+ * @author 王熙楠 wangxn@tiansu-china.com
+ */
+public class DefaultConstructor {
+
+    public DefaultConstructor() {
+        System.out.println("调用无参构造器");
+    }
+
+    public static void main(String[] args) {
+
+        DefaultConstructor defaultConstructor = new DefaultConstructor();
+    }
+
+}
