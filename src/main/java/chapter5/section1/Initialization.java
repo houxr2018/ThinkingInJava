2e6c55a442dd93e5251357e6f2ece60ae1b39c16
wangxn <704801812@qq.com>
2017-10-16 10:00:27 +0800

    练习5.1

---
+++ src/main/java/chapter5/section1/Initialization.java
@@ -0,0 +1,18 @@
+package chapter5.section1;
+
+/**
+ * Description: 初始化类的String引用
+ * CreateTime: 2017/10/16 9:52
+ *
+ * @author 王熙楠 wangxn@tiansu-china.com
+ */
+public class Initialization {
+
+    private String str;
+
+    public static void main(String[] args) {
+
+        Initialization initialization = new Initialization();
+        System.out.println(initialization.str);
+    }
+}
