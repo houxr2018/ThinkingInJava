d04ef90f1c43a8fd094dad43ba9df8b36642e94c
wangxn <704801812@qq.com>
2017-10-16 14:55:23 +0800

    练习5.7

---
+++ src/main/java/chapter5/section7/SynthesizedConstructor.java
@@ -0,0 +1,15 @@
+package chapter5.section7;
+
+/**
+ * Description: 自动生成默认构造器
+ * CreateTime: 2017/10/16 14:53
+ *
+ * @author 王熙楠 wangxn@tiansu-china.com
+ */
+public class SynthesizedConstructor {
+
+    public static void main(String[] args) {
+
+        new SynthesizedConstructor();
+    }
+}
