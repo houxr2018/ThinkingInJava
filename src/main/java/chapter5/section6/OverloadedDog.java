5b2a483af252afc02d050868e26e64d06a28685b
wangxn <704801812@qq.com>
2017-10-16 14:40:08 +0800

    练习5.6

--- src/main/java/chapter5/section6/OverloadedDog.java
+++ src/main/java/chapter5/section6/OverloadedDog.java
@@ -1,27 +1,28 @@
-package chapter5.section5;
+package chapter5.section6;
 
 /**
- * Description: 重载bark()方法
+ * Description: bark()的重载方法接受两个参数顺序不同
  * CreateTime: 2017/10/16 14:19
  *
  * @author 王熙楠 wangxn@tiansu-china.com
  */
 class Dog {
 
-    public void bark(int i) {
+    public void bark(int i, char ch) {
         System.out.println("barking");
     }
 
-    public void bark(char ch) {
+    public void bark(char ch, int i) {
         System.out.println("howling");
     }
 }
+
 public class OverloadedDog {
 
     public static void main(String[] args) {
 
         Dog dog = new Dog();
-        dog.bark(1);
-        dog.bark('1');
+        dog.bark(1, '1');
+        dog.bark('1', 1);
     }
 }
